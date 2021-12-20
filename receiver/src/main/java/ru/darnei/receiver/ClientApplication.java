a7c62c01641e5663f089aba9200b11244ec1f173
darnei3 <darnei33@mail.ru>
2021-12-20 20:52:04 +0300

    - Добавлен клиент на Feign - Swagger подключён к клиенту

--- receiver/src/main/java/ru/darnei/receiver/ClientApplication.java
+++ receiver/src/main/java/ru/darnei/receiver/ClientApplication.java
@@ -2,8 +2,10 @@ package ru.darnei.receiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+@EnableFeignClients
 @SpringBootApplication
 public class ClientApplication {
     public static void main(String[] args) { SpringApplication.run(ClientApplication.class, args); }
--- receiver/src/main/java/ru/darnei/receiver/controller/SettingController.java
+++ receiver/src/main/java/ru/darnei/receiver/controller/SettingController.java
@@ -1,23 +1,38 @@
 package ru.darnei.receiver.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import ru.darnei.receiver.model.Setting;
-
-import java.util.Set;
+import ru.darnei.receiver.service.SettingClient;
 
 @RestController
+@Api("Контроллер для вызова запросов сервера")
 public class SettingController {
 
+private final SettingClient settingClient;
+    public SettingController(SettingClient settingClient) {
+        this.settingClient = settingClient;
+    }
 
     @GetMapping
-    @RequestMapping("/template")
-    public Setting getSetting(){
+    @RequestMapping(value = "/template", method = RequestMethod.GET)
+    @ApiOperation("Получить все настройки от сервера с помощью RestTemplate")
+    public Setting getSettingRestTemplate(){
         RestTemplate template = new RestTemplate();
         return template.getForObject("http://localhost:8080/setting", Setting.class);
+    }
+
+    @GetMapping
+    @RequestMapping( value = "/feign", method = RequestMethod.GET)
+    @ApiOperation("Получить все настройки от сервера с помощью Feign")
+    public Setting getSettingFeign(){
+        return settingClient.getSettings();
     }
 
 
